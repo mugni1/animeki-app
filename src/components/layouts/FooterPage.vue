17f184e608c750350d4bec4faa9d85b5536ec236
mugni1 <asepabdulmugni22@gmail.com>
2025-08-11 12:46:42 +0700

    refactor: footer

--- src/components/layouts/FooterPage.vue
+++ src/components/layouts/FooterPage.vue
@@ -1,7 +1,10 @@
 <template>
   <footer class="w-full py-5 mt-5 bg-slate-900/5 dark:bg-white/5">
-    <div class="container px-3 text-center mx-auto text-xs md:text-base">
-      Copyright &copy;{{ new Date().getFullYear() }} By AnuDev
+    <div class="container px-3 text-center mx-auto">
+      <span class="text-sm md:text-base font-bold">
+        Copyright &copy;{{ new Date().getFullYear() }} Animeki. All rights reserved</span
+      >
+      <p class="text-xs md:text-sm">Made with ❤️ by AnuDev</p>
     </div>
   </footer>
 </template>
