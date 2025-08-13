9c947f874a5b7285bade1dcd3ad45163f7d60172
mugni1 <asepabdulmugni22@gmail.com>
2025-08-13 14:06:28 +0700

    feat: implementation base pagination

--- src/views/OngoingView.vue
+++ src/views/OngoingView.vue
@@ -13,26 +13,15 @@
         icon="pi pi-clock"
         title="Ongoing anime"
       />
-
-      <Paginator
-        class="mt-5"
-        :rows="1"
-        :totalRecords="totalRecords"
-        :first="page - 1"
-        :pageLinkSize="pageLinkSize"
-        @page="onPageChange"
-      >
-      </Paginator>
+      <BasePagination :total-records="totalRecords" v-model:page="page" />
     </BaseContainer>
-
     <FooterPage />
   </div>
 </template>
 
 <script setup lang="ts">
 import { useGetOngoingAnime } from '@/hooks/useGetAnimeOngoing'
-import { Paginator, type PageState } from 'primevue'
-import { computed, onBeforeUnmount, onMounted, ref, watch, watchEffect } from 'vue'
+import { computed, ref, watch, watchEffect } from 'vue'
 
 // components
 import ErrorPage from '@/components/layouts/ErrorPage.vue'
@@ -40,56 +29,22 @@ import FooterPage from '@/components/layouts/FooterPage.vue'
 import LoadingPage from '@/components/layouts/LoadingPage.vue'
 import BaseContainer from '@/components/shared/BaseContainer.vue'
 import ListAnime from '@/components/shared/List/ListAnime.vue'
+import BasePagination from '@/components/layouts/BasePagination.vue'
 
 // state
 const page = ref(1)
 const totalRecords = ref(0)
-const pageLinkSize = ref(6) // default untuk layar besar
 const { data, error, isError, isPending, refetch } = useGetOngoingAnime(
   computed(() => page.value.toString()),
 )
 
-// methods
-function onPageChange(event: PageState) {
-  page.value = event.page + 1
-}
-
-function updatePageLinkSize() {
-  if (window.innerWidth < 640) {
-    pageLinkSize.value = 3
-  } else if (window.innerWidth >= 640 && window.innerWidth < 1024) {
-    pageLinkSize.value = 6
-  } else {
-    pageLinkSize.value = 10
-  }
-}
-
 // watchers
-watch(
-  () => page.value,
-  () => {
-    refetch() // Query otomatis pakai page terbaru
-  },
-)
+watch(page, () => {
+  refetch()
+})
 watchEffect(() => {
   if (page.value === 1 && data.value?.meta_data.total_records) {
     totalRecords.value = data.value.meta_data.total_records
   }
 })
-
-// mounted dan sebelum UnMounted
-onMounted(() => {
-  updatePageLinkSize()
-  window.addEventListener('resize', updatePageLinkSize)
-})
-onBeforeUnmount(() => {
-  window.removeEventListener('resize', updatePageLinkSize)
-})
 </script>
-
-<style scoped>
-:deep(.p-paginator) {
-  background-color: transparent;
-  border: 1px solid #10b981; /* emerald-500 */
-}
-</style>
